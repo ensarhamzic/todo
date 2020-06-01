bfd0521fc051d24df77f8fcfc0dd1e3d7b8cc0e0
ensarhamzic01 <ensarhamzic01@gmail.com>
2020-06-01 18:16:46 +0200

    Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ensar
                 </div>
 
                 <div class="links">
